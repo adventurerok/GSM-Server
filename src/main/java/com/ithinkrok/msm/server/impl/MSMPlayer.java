71e4f1fa92d0670fccd32369e7d646ddf3126dc3
paul <pauldurbz@gmail.com>
2016-02-15 22:06:13 +0000

    Log ChangeServer requests

--- src/main/java/com/ithinkrok/msm/server/impl/MSMPlayer.java
+++ src/main/java/com/ithinkrok/msm/server/impl/MSMPlayer.java
@@ -6,6 +6,8 @@ import com.ithinkrok.msm.server.Player;
 import com.ithinkrok.util.config.Config;
 import com.ithinkrok.util.config.MemoryConfig;
 import org.apache.commons.lang.Validate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.UUID;
 
@@ -13,6 +15,8 @@ import java.util.UUID;
  * Created by paul on 06/02/16.
  */
 public class MSMPlayer implements Player {
+
+    private static final Logger log = LogManager.getLogger(MSMPlayer.class);
 
     private final UUID uuid;
     private String name;
@@ -40,21 +44,6 @@ public class MSMPlayer implements Player {
     }
 
     @Override
-    public void sendMessage(String message) {
-        if(!isConnected()) return;
-
-        minecraftServer.messagePlayers(message, this);
-    }
-
-    public boolean isConnected() {
-        return minecraftServer != null && minecraftServer.getConnection() != null;
-    }
-
-    private Channel getAPIChannel() {
-        return minecraftServer.getConnection().getChannel("MSMAPI");
-    }
-
-    @Override
     public MinecraftServer getServer() {
         return minecraftServer;
     }
@@ -67,8 +56,10 @@ public class MSMPlayer implements Player {
     public void changeServer(MinecraftServer newServer) {
         Validate.notNull(newServer, "newServer cannot be null");
 
-        if(!minecraftServer.hasBungee() || !newServer.hasBungee()){
-            throw new UnsupportedOperationException("Cannot change a player between non bungee servers");
+        log.trace("ChangeServer player \"" + name + "\": " + minecraftServer.getName() + " -> " + newServer.getName());
+        if (!minecraftServer.hasBungee() || !newServer.hasBungee()) {
+            log.debug("Blocked ChangeServer request as one of the servers involved does not support bungee");
+            return;
         }
 
         Config payload = new MemoryConfig();
@@ -78,5 +69,20 @@ public class MSMPlayer implements Player {
         payload.set("mode", "ChangeServer");
 
         getAPIChannel().write(payload);
+    }
+
+    private Channel getAPIChannel() {
+        return minecraftServer.getConnection().getChannel("MSMAPI");
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        if (!isConnected()) return;
+
+        minecraftServer.messagePlayers(message, this);
+    }
+
+    public boolean isConnected() {
+        return minecraftServer != null && minecraftServer.getConnection() != null;
     }
 }
