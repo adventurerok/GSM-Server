c31e79696771089563f281211109e58429be970e
paul <pauldurbz@gmail.com>
2016-03-10 18:38:53 +0000

    Start work on a better resource updating base protocol

---
+++ src/main/java/com/ithinkrok/msm/server/protocol/ServerUpdateBaseProtocol.java
@@ -0,0 +1,236 @@
+package com.ithinkrok.msm.server.protocol;
+
+import com.ithinkrok.msm.common.Channel;
+import com.ithinkrok.msm.common.util.io.DirectoryListener;
+import com.ithinkrok.msm.server.Connection;
+import com.ithinkrok.msm.server.Server;
+import com.ithinkrok.msm.server.ServerListener;
+import com.ithinkrok.util.config.Config;
+import com.ithinkrok.util.config.MemoryConfig;
+import com.ithinkrok.util.config.YamlConfigIO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.time.Instant;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by paul on 10/03/16.
+ */
+public abstract class ServerUpdateBaseProtocol implements ServerListener, DirectoryListener {
+
+    private final Logger log = LogManager.getLogger(ServerUpdateBaseProtocol.class);
+
+    /**
+     * A map of resource config paths to resources.
+     */
+    private final Map<Path, ServerResource> serverResources = new ConcurrentHashMap<>();
+
+    /**
+     * A map of client names to maps of resource names to resource versions.
+     */
+    private final Map<String, Map<String, Instant>> clientResources = new ConcurrentHashMap<>();
+
+    private final Map<Path, Future<?>> modifiedPaths = new ConcurrentHashMap<>();
+
+    private Server server;
+    private final Path serverResourcePath;
+
+    public ServerUpdateBaseProtocol(Path serverResourcePath) {
+        this.serverResourcePath = serverResourcePath;
+
+        if(!Files.exists(serverResourcePath)) {
+            try {
+                Files.createDirectories(serverResourcePath);
+            } catch (IOException e) {
+                log.warn("Failed to create missing updated resource directory", e);
+                return;
+            }
+        }
+
+        updateResourceVersions(serverResourcePath);
+    }
+
+    private void updateResourceVersions(Path serverResourcePath) {
+        //TODO symlinks
+
+        try(DirectoryStream<Path> directoryStream = Files.newDirectoryStream(serverResourcePath)) {
+            for(Path path : directoryStream) {
+                updateResource(path);
+            }
+        } catch (IOException e) {
+            log.warn("Failed to do full resource updates directory check", e);
+        }
+    }
+
+    /**
+     *
+     * @param path The path of the resource or resource config to update
+     * @return If the resource or resource config was updated
+     * @throws IOException If an error occurs while reading the resource info
+     */
+    private boolean updateResource(Path path) throws IOException {
+        Config config = getResourceConfig(path);
+
+        if(config == null) {
+            //Ensure that this resource does not have a provided config.
+
+            String fileName = path.getFileName().toString();
+            String configName = fileName + ".0";
+            Path configPath = path.getParent().resolve(configName);
+
+            if(Files.exists(configPath)){
+                return false;
+            }
+
+            config = getDefaultResourceConfig(path);
+        }
+
+        updateResourceFromConfig(path, config);
+
+        return true;
+    }
+
+    private void updateResourceFromConfig(Path configPath, Config config) throws IOException {
+        ServerResource resource = new ServerResource();
+
+        resource.name = config.getString("resource_name");
+        resource.path = serverResourcePath.resolve(config.getString("resource_path"));
+
+        Instant modified = getModifiedInstant(resource.path);
+        if(modified == null) return;
+
+        resource.modified = modified;
+
+        serverResources.put(configPath, resource);
+
+        //TODO update client versions
+    }
+
+    private void resourceModified(Path resourcePath) throws IOException {
+        if(updateResource(resourcePath)) return;
+
+        for(Map.Entry<Path, ServerResource> resourceEntry : serverResources.entrySet()) {
+            if(!resourceEntry.getValue().path.equals(resourcePath)) continue;
+
+            updateResource(resourceEntry.getKey());
+        }
+    }
+
+    protected Instant getModifiedInstant(Path path) throws IOException {
+        return Files.getLastModifiedTime(path).toInstant();
+    }
+
+    protected Config getDefaultResourceConfig(Path path) {
+        Config config = new MemoryConfig();
+
+        String fileName = path.getFileName().toString();
+        config.set("resource_name", fileName);
+        config.set("resource_path", path.toString());
+
+        return config;
+    }
+
+    private Config getResourceConfig(Path path) throws IOException {
+        String fileName = path.getFileName().toString().toLowerCase();
+
+        int dotIndex = fileName.lastIndexOf('.');
+        if(dotIndex < 0) return null;
+
+        String extension = fileName.substring(dotIndex + 1, fileName.length());
+        if(extension.isEmpty()) return null;
+
+        try{
+            //Check the extension is an integer. This means it is a config file for a resource
+            //noinspection ResultOfMethodCallIgnored
+            Integer.parseInt(extension);
+
+            Config config = YamlConfigIO.loadToConfig(path, new MemoryConfig());
+
+            config.set("resource_name", fileName.substring(0, dotIndex));
+            return config;
+        } catch (NumberFormatException ignored) {
+            return null;
+        }
+    }
+
+    @Override
+    public void connectionClosed(Connection connection) {
+
+    }
+
+    @Override
+    public void packetRecieved(Connection connection, Channel channel, Config payload) {
+
+    }
+
+    @Override
+    public void connectionOpened(Connection connection, Channel channel) {
+
+    }
+
+    @Override
+    public void serverStopped(Server server) {
+
+    }
+
+    @Override
+    public void serverStarted(Server server) {
+        this.server = server;
+
+        //TODO listen in subdirectories
+        server.getDirectoryWatcher().registerListener(serverResourcePath, this);
+    }
+
+    @Override
+    public void fileChanged(Path changed, WatchEvent.Kind<?> event) {
+        if(Files.isDirectory(changed) && event == StandardWatchEventKinds.ENTRY_CREATE) {
+            server.getDirectoryWatcher().registerListener(changed, this);
+        } else if(event == StandardWatchEventKinds.ENTRY_DELETE) {
+            server.getDirectoryWatcher().unregisterAllListeners(changed);
+
+            //Remove all resources that start with this resource path
+            //If this is a directory, it removes all resources in this directory or subdirectories.
+            //If this is a file, it removes this resource.
+            Iterator<Map.Entry<Path, ServerResource>> iterator = serverResources.entrySet().iterator();
+            while(iterator.hasNext()) {
+                Map.Entry<Path, ServerResource> next = iterator.next();
+
+                if(next.getKey().startsWith(changed)) {
+                    log.info("Removing resource " + next.getKey() + " as it's directory was deleted");
+                    iterator.remove();
+                }
+            }
+        }
+
+        if(Files.isDirectory(changed)) return;
+
+        //Wait 5 seconds before reading the change to allow time for file to be fully uploaded.
+        Future<?> oldFuture = modifiedPaths.get(changed);
+        if (oldFuture != null) oldFuture.cancel(false);
+
+        Future<?> newFuture = server.scheduleAsync(() -> {
+            try {
+                resourceModified(changed);
+            } catch (IOException e) {
+                log.warn("Failed to update resource: " + changed, e);
+            }
+            modifiedPaths.remove(changed);
+        }, 5, TimeUnit.SECONDS);
+
+        modifiedPaths.put(changed, newFuture);
+    }
+
+    private static class ServerResource {
+        String name;
+        Path path;
+
+        Instant modified;
+    }
+}
